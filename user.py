563a5497334aa00df9558e7d6f38917059363d2e
Harry Mellsop <harrymellsop@gmail.com>
2019-05-19 16:58:24 -0700

    Resolved threading issues

--- user.py
+++ user.py
@@ -2,7 +2,7 @@ import numpy as np
 from pynput import keyboard
 from pynput import mouse
 import datetime
-import timeit
+import time
 
 def welcomeUser():
     print("Welcome to Alex, Harry and Ryan's CS221 Project")
@@ -22,29 +22,39 @@ previousKey = (None, None,                  None)
 currentKeyInfo = (None, None)
 
 def push_down(key):
+    global previousKey
+    global currentKeyInfo
+    global sparse_vector
     # filter out auxillary key presses (shift, capslock, etc.)
     try:
         print('Standard alphanumeric key {0} pressed'.format(key.char))
-        currentKeyDepressedTime = timeit.timeit()
+        currentKeyDepressedTime = time.time()
         print(previousKey)
         if previousKey != (None, None, None):
             # this is not the first key press - generate data that relies on prev
             print("    not first key")
         # generate data that is just reliant on this key press
         print("Updating this bad boy")
-        print("HELLLLO")
-        currentKeyInfo = (key.char, timeit.timit())
-        print("Chicken sandwich: {0}".format(currentKeyInfo))
+        
+        currentKeyInfo = (key.char, time.time())
+        
     except AttributeError: print('special key {0} pressed'.format(key))
 
 def release(key):
+    global previousKey
+    global currentKeyInfo
+    global sparse_vector
+    print("")
+    print("")
+    print("")
+    print("")
     if key == keyboard.Key.esc:
         print("Terminating.")
         # Stop listener
         return False
     print(currentKeyInfo)
-    if currentKeyInfo[1] is not None:
-        print('{0} released after {1} seconds'.format(key.char, timeit.timeit() - currentKeyInfo[1]))
+    if currentKeyInfo[1] != None:
+        print('{0} released after {1} seconds'.format(key.char, time.time() - currentKeyInfo[1]))
 
 with keyboard.Listener(on_press=push_down, on_release=release) as listener:
     listener.join()
