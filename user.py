6435e454e7d3747f9498b36be1645c0757ea7ade
Harry Mellsop <harrymellsop@gmail.com>
2019-05-19 16:11:46 -0700

    Beginning to implement basic keylogging

--- user.py
+++ user.py
@@ -1,29 +1,48 @@
-#import pynput
 import numpy as np
 from pynput import keyboard
 from pynput import mouse
+import datetime
+import timeit
 
 def welcomeUser():
     print("Welcome to Alex, Harry and Ryan's CS221 Project")
-    print("Please enter your password (hint, it's \".tie5Roan1\") ")
+    print("Please enter your password (hint, it's \".tie5Roanl\") ")
+    print("If you're a bitch, press esc to exit")
+
+welcomeUser()
+
+# create the sparse vector to be populated
+sparse_vector = {}
+#          what key | time pressed down  | time released (if it exists)
+#               v      v                      v
+previousKey = (None, None,                  None)
+
+#            what key | time pressed down
+#                  v      v
+currentKeyInfo = (None, None)
 
 def push_down(key):
+    # filter out auxillary key presses (shift, capslock, etc.)
     try:
-        print('alphanumeric key {0} pressed'.format(
-            key.char))
-    except AttributeError:
-        print('special key {0} pressed'.format(
-            key))
+        print('Standard alphanumeric key {0} pressed'.format(key.char))
+        currentKeyDepressedTime = timeit.timeit()
+        if previousKey is not (None, None, None):
+            # this is not the first key press - generate data that relies on prev
+            print "not first key"
+        # generate data that is just reliant on this key press
+        currentKeyInfo = (key.char, timeit.timit())
+        
+        
+    except AttributeError: print('special key {0} pressed'.format(key))
 
 def release(key):
     if key == keyboard.Key.esc:
-        # Terminating.
+        print("Terminating.")
         # Stop listener
         return False
-    print('{0} released'.format(
-        key))
+    if key.char
+    print('{0} released'.format(key))
 
-welcomeUser()
 with keyboard.Listener(on_press=push_down, on_release=release) as listener:
     listener.join()
 
