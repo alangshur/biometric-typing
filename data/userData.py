a5b41885d8e7c194a1303d89c6f007361e1efbe9
Ryan Kearns <kearns@stanford.edu>
2019-06-01 21:33:35 -0700

    more data!!

--- data/userData.py
+++ data/userData.py
@@ -7,19 +7,19 @@ import data
 import pickle
 
 def getUserDataFeatures():
-	fileRead = open('data/user-password-data.csv', 'rb')
+	fileRead = open('data/user-password-data.txt', 'rb')
 	userDataFeatures = pickle.load(fileRead)
 	return userDataFeatures
 
 def main():
 	# load previous data
-	fileRead = open('data/user-password-data.csv', 'rb')
+	fileRead = open('data/user-password-data.txt', 'rb')
 	theResurrection = pickle.load(fileRead)
 	# prompt user and generate raw feature outputs, akin to CSV file
 	userData = userInterface.welcomeUserAndCollectUserPasswordData(10, 0)
 	for datum in userData:
 		theResurrection.append(data.getFeaturesFromList(datum))
-	file = open('data/user-password-data.csv', 'wb')
+	file = open('data/user-password-data.txt', 'wb')
 	pickle.dump(theResurrection, file)
 
 if __name__ == '__main__':
